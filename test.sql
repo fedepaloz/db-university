4ca7a94c0770624bfb5f13fdc72ee4c863e327e1
fedepaloz <palozzifederico@gmail.com>
2022-09-06 16:07:11 +0200

    4 select

--- test.sql
+++ test.sql
@@ -23,7 +23,7 @@ WHERE `cfu` > '10';
 SELECT * 
 FROM `students` 
 WHERE YEAR(`date_of_birth`) > 1991;
---è più corretto così ?
+--è più corretto così
 SELECT * 
 FROM `students` 
 WHERE `date_of_birth` > '1991-09-06';
@@ -32,6 +32,18 @@ WHERE `date_of_birth` > '1991-09-06';
 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+
+SELECT * 
+FROM `courses` 
+WHERE `period` 
+LIKE 'I s%' 
+AND `year` = '1';
+--oppure
+
+SELECT * 
+FROM `courses` 
+WHERE `period` = 'I semestre' 
+AND `year`= '1';
 
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
