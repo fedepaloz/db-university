f57b168f947d9b55c21b16bd0e66e8221da051f5
fedepaloz <palozzifederico@gmail.com>
2022-09-06 16:42:28 +0200

    1 group

--- test.sql
+++ test.sql
@@ -79,6 +79,9 @@ IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
+SELECT COUNT(*), `enrolment_date` 
+FROM `students` 
+GROUP BY `enrolment_date`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
