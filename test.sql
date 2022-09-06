5a8f916bf7f42471b7e7aab410f52d751636df7c
fedepaloz <palozzifederico@gmail.com>
2022-09-06 17:28:10 +0200

    finished

--- test.sql
+++ test.sql
@@ -85,8 +85,19 @@ GROUP BY `enrolment_date`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
+SELECT COUNT(*), `office_address` 
+FROM `teachers` 
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
 
+SELECT COUNT(*),AVG(vote) 
+FROM `exam_student` 
+GROUP BY `exam_id`;
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(*),`department_id`
+FROM `degrees` 
+GROUP BY `department_id`;
