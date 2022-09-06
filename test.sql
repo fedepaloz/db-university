58f0192acfdb72141047b9c84d91b3bb04d57408
fedepaloz <palozzifederico@gmail.com>
2022-09-06 15:24:54 +0200

    1.2 select

--- test.sql
+++ test.sql
@@ -1,9 +1,14 @@
 -- ---------------------------------------  QUERY CON SELECT
 
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
+
 SELECT * 
 FROM `students` 
 WHERE `date_of_birth` LIKE '1990-%';
+--seconda possibilità
+
+SELECT * 
+FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
 
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
