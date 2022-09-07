88c4b43791834999429360aee5512d628d980c7f
fedepaloz <palozzifederico@gmail.com>
2022-09-07 18:15:28 +0200

    finished

--- DB University JOIN.sql
+++ DB University JOIN.sql
@@ -29,13 +29,41 @@ AND `teachers`.`surname`= 'amato'
 
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
-
+SELECT `students`.`surname`,`students`.`name`,`departments`.`name`,`degrees`.`name`
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname` ASC;
 
 
 
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
+SELECT `degrees`.`name`,`courses`.`name`,`teachers`.`name`,`teachers`.`surname`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id`=`course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees`
+ON `degrees`.`id`= `courses`.`degree_id`;
+
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+SELECT `teachers`.`name`,`teachers`.`surname`,`departments`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id`=`course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees`
+ON `degrees`.`id`= `courses`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'dipartimento di matematica';
 
 
 --7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esame
+
